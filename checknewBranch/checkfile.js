0646944710827a85f4c665a86d71dc23883901bf
dev.Waseem06 <dev.waseem06@gmail.com>
2026-09-10 13:08:28 -0700

    added some tex

--- checknewBranch/checkfile.js
+++ checknewBranch/checkfile.js
@@ -1,1 +1,5 @@
-hello
+// ok right to connect account you sue config
+// to connect curr to onlune repo use url , add remote origin
+// once push all the project , 
+// code , git add new changes , git status , commit it
+// push to the github repo 
